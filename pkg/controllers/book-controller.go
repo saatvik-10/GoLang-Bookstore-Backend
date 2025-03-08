f724b2f84b65dfbb3e187d557b7ad1534ae77ff2
saatvik-10 <saatvikmadan529@gmail.com>
2025-03-08 21:38:30 +0530

    added GetBook controller

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,2 +1,25 @@
 package controllers
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+	"github.com/saatvik-10/bookstore/pkg/utils"
+	"github.com/saatvik-10/bookstore/pkg/models"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks() //fetch books
+	res, _ := json.Marshal(newBooks) //like "application/json" in nodejs
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK) //200
+	_, WriteErr := w.Write(res)  //to send to frontend/postman
+
+	if WriteErr != nil {
+		fmt.Println("Error", WriteErr)
+	}
+}
