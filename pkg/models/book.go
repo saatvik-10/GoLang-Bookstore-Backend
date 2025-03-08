dfe2d5de30a0ba2a70725f5f1a4df2b160891cfc
saatvik-10 <saatvikmadan529@gmail.com>
2025-03-08 16:09:09 +0530

    created Post route

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -9,8 +9,8 @@ var db *gorm.DB
 
 type Book struct {
 	gorm.Model
-	Name string `gorm:"" json:"name"`
-	Author string `json:"author"`
+	Name        string `gorm:"" json:"name"`
+	Author      string `json:"author"`
 	Publication string `json:"publication"`
 }
 
@@ -18,4 +18,10 @@ func init() {
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
 }
