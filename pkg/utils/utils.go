7d29d7c7f4f12acf32be0d35d60b7c2bf6ee8704
saatvik-10 <saatvikmadan529@gmail.com>
2025-03-08 12:51:17 +0530

    added data unmarshalling

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,2 +1,20 @@
+//this file will unmarshal the json
+
 package utils
 
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+//Client sends a request → r *http.Request reads it
+//Server sends a response → w http.ResponseWriter writes it
+
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := io.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return
+		}
+	}
+}
