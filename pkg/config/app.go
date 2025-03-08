a8d7daaa897d9e6330eff36bf892213d6964ce7c
saatvik-10 <saatvikmadan529@gmail.com>
2025-03-08 11:52:54 +0530

    added sql config

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,2 +1,22 @@
-package main
+package config
 
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect () {
+	d, err := gorm.Open("mysql", "saatvik:saatvik@1210/bookstore?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDb() *gorm.DB {
+	return db
+}
