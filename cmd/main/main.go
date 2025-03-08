51f019e3381dd37bb65cf4ef387597f69fd398bc
saatvik-10 <saatvikmadan529@gmail.com>
2025-03-08 16:02:16 +0530

    server setup

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/saatvik-10/bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+
+	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
